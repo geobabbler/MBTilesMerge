f73822ad09f971b0a968a03f30edf562d5f8f2ad
Bill Dollins <bill@geomusings.com>
2014-03-31 12:19:19 -0400

    initial commit

---
+++ MBTilesMerge/frmMain.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace MBTilesMerge
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.tmrUI = new System.Windows.Forms.Timer(this.components);
+            this.ofdMBTiles = new System.Windows.Forms.OpenFileDialog();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.txtInstructions = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MBTilesMerge.Properties.Resources.mbtiles;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.DisplayMember = "FileName";
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(214, 5);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(195, 199);
+            this.lstFiles.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(456, 3);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(176, 24);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Add MBTiles Database...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Enabled = false;
+            this.btnMoveUp.Font = new System.Drawing.Font("Symbol", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnMoveUp.Location = new System.Drawing.Point(415, 5);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(30, 26);
+            this.btnMoveUp.TabIndex = 3;
+            this.btnMoveUp.Text = "­";
+            this.btnMoveUp.UseVisualStyleBackColor = true;
+            this.btnMoveUp.Click += new System.EventHandler(this.btnMoveUp_Click);
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.Enabled = false;
+            this.btnMoveDown.Font = new System.Drawing.Font("Symbol", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnMoveDown.Location = new System.Drawing.Point(415, 60);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(30, 26);
+            this.btnMoveDown.TabIndex = 4;
+            this.btnMoveDown.Text = "¯";
+            this.btnMoveDown.UseVisualStyleBackColor = true;
+            this.btnMoveDown.Click += new System.EventHandler(this.btnMoveDown_Click);
+            // 
+            // tmrUI
+            // 
+            this.tmrUI.Interval = 25;
+            this.tmrUI.Tick += new System.EventHandler(this.tmrUI_Tick);
+            // 
+            // ofdMBTiles
+            // 
+            this.ofdMBTiles.Filter = "MBTiles Databases|*.mbtiles|All files|*.*";
+            this.ofdMBTiles.Title = "Select MBTiles Database";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(547, 179);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(85, 24);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(456, 179);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(85, 24);
+            this.btnMerge.TabIndex = 6;
+            this.btnMerge.Text = "Merge...";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // txtInstructions
+            // 
+            this.txtInstructions.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInstructions.Location = new System.Drawing.Point(456, 41);
+            this.txtInstructions.Multiline = true;
+            this.txtInstructions.Name = "txtInstructions";
+            this.txtInstructions.ReadOnly = true;
+            this.txtInstructions.Size = new System.Drawing.Size(176, 125);
+            this.txtInstructions.TabIndex = 7;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 207);
+            this.Controls.Add(this.txtInstructions);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMoveDown);
+            this.Controls.Add(this.btnMoveUp);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.Text = "MBTiles Merge Utility";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnMoveDown;
+        private System.Windows.Forms.Timer tmrUI;
+        private System.Windows.Forms.OpenFileDialog ofdMBTiles;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.TextBox txtInstructions;
+    }
+}
+
---
+++ MBTilesMerge/frmMain.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MBTilesMerge
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrUI_Tick(object sender, EventArgs e)
+        {
+            validateUI();
+        }
+
+        private void validateUI()
+        {
+            if (lstFiles.Items.Count > 1)
+            {
+                if (lstFiles.SelectedItem != null)
+                {
+                    if (lstFiles.SelectedIndex > 0)
+                    {
+                        this.btnMoveUp.Enabled = true;
+                    }
+                    else
+                    {
+                        this.btnMoveUp.Enabled = false;
+                    }
+                    if (lstFiles.SelectedIndex < (lstFiles.Items.Count - 1))
+                    {
+                        this.btnMoveDown.Enabled = true;
+                    }
+                    else
+                    {
+                        this.btnMoveDown.Enabled = false;
+                    }
+                }
+                else
+                {
+                    this.btnMoveUp.Enabled = false;
+                    this.btnMoveDown.Enabled = false;
+                }
+                this.btnMerge.Enabled = true;
+            }
+            else
+            {
+                this.btnMoveUp.Enabled = false;
+                this.btnMoveDown.Enabled = false;
+                this.btnMerge.Enabled = false;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.tmrUI.Start();
+            this.txtInstructions.Text = "NOTE: Order matters!\r\n\r\nDatabases will be merged from the bottom up. Duplicate records in a lower database will be replace by those in a higher database.\r\n\r\nEnsure proper order before merging!";
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            this.ofdMBTiles.ShowDialog();
+            if (this.ofdMBTiles.FileName != "")
+            {
+                var fn = new FileItem() { FileName = System.IO.Path.GetFileName(this.ofdMBTiles.FileName), FullPath = this.ofdMBTiles.FileName };
+                this.lstFiles.Items.Add(fn);
+            }
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            var item = this.lstFiles.SelectedItem;
+            var idx = this.lstFiles.SelectedIndex;
+
+            this.lstFiles.Items.Remove(item);
+            this.lstFiles.Items.Insert(idx - 1, item);
+            this.lstFiles.SelectedItem = item;
+        }
+
+        private void btnMoveDown_Click(object sender, EventArgs e)
+        {
+            var item = this.lstFiles.SelectedItem;
+            var idx = this.lstFiles.SelectedIndex;
+
+            this.lstFiles.Items.Remove(item);
+            this.lstFiles.Items.Insert(idx + 1, item);
+            this.lstFiles.SelectedItem = item;
+        }
+
+        private void mergeDatabases(string src, string dest)
+        {
+            //echo PRAGMA journal_mode=PERSIST;PRAGMA page_size=80000;PRAGMA synchronous=OFF;ATTACH DATABASE '%1' AS source;REPLACE INTO tiles SELECT * FROM source.tiles;REPLACE INTO grids SELECT * FROM source.grids;REPLACE INTO grid_data SELECT * FROM source.grid_data; | "C:\spatialite3\bin\sqlite3.exe" %2%
+            string connstr = "Data Source={0};Version=3;";
+            string sql = "PRAGMA journal_mode=PERSIST;" +
+                "PRAGMA page_size=80000;" +
+                "PRAGMA synchronous=OFF;" +
+                "ATTACH DATABASE '{0}' AS source;" +
+                "REPLACE INTO map SELECT * FROM source.map;" +
+                "REPLACE INTO grid_key SELECT * FROM source.grid_key;" +
+                "REPLACE INTO images SELECT * FROM source.images;" +
+                "REPLACE INTO grid_utfgrid SELECT * FROM source.grid_utfgrid;";
+
+            sql = string.Format(sql, src);
+            connstr = string.Format(connstr, dest);
+
+            try
+            {
+
+                using (var conn = new System.Data.SQLite.SQLiteConnection(connstr))
+                {
+                    conn.Open();
+                    using (var cmd = new System.Data.SQLite.SQLiteCommand(conn))
+                    {
+                        cmd.CommandText = sql;
+                        var res = cmd.ExecuteScalar();
+                    }
+                    conn.Close();
+                }
+            }
+            catch { }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            var proceed = MessageBox.Show("This process will make changes to your data! It is recommended that you make a backup before proceeding! Do you wish to continue?", "Pay Attention!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (proceed == System.Windows.Forms.DialogResult.Yes)
+            {
+                var dest = this.lstFiles.Items[this.lstFiles.Items.Count - 1] as FileItem;
+                int start = this.lstFiles.Items.Count - 2;
+                for (int i = start; i >= 0; i--)
+                {
+                    var src = this.lstFiles.Items[i] as FileItem;
+                    mergeDatabases(src.FullPath, dest.FullPath);
+                }
+            }
+        }
+    }
+
+    class FileItem
+    {
+        public string FileName { get; set; }
+        public string FullPath { get; set; }
+    }
+}
